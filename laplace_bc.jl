a344b3618b5f09fdaec8588e134adb2042b17d5f
Phoebe Grosser <phoebegrosser@Phoebes-MacBook-Pro.local>
2023-01-12 15:42:45 +1100

    Work on BCs

--- laplace_bc.jl
+++ laplace_bc.jl
@@ -1,23 +1,42 @@
 using LinearAlgebra
 using OffsetArrays
+using SparseArrays
 
 Lx = pi
-nx = 10
+nx = 5 # Ordinary points
 hx = 1 # Halo points
 dx = Lx/(nx + hx - 1)
 x = (-hx*dx):dx:Lx
+x = OffsetArray(x, -2)
 
-f = cos.(5*pi/(2*Lx).*x)
+# Only applied to interior
+f = cos.(5*pi/(2*Lx).*x[0:nx])
 
-# Doing it the same way as previously (with basis vectors)
-function laplacian(n_x,e)
+p = spzeros(nx,1)
 
-    p = zeros(n_x,1)
+# To check -> if BC is applied to all basis vectors, it will also be applied to eta?
 
-    for k in 1:n_x
-        p[k] = (e[k + 1] + e[k - 1] - 2*e[k])./(dx^2)
+function laplacian!(p, e, nx)
+
+    p .= 0
+
+    for k in 0:(nx - 1)
+        p[k] = (e[k + 1] + e[k - 1] - 2*e[k])/(dx^2)
     end
 
-    # Need to introduce BCs here -> if it applies to the basis vectors, it will apply to the eta vector?
+    return nothing
+end
+
+function dirichlet_bc(nx)
 
 end
+
+function neumann_bc(nx)
+
+end
+
+e = zeros(nx + hx)
+
+function generate_A!(A, nx, e)
+
+
