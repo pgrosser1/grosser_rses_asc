0544ba6ccc9af53d53b5b44336ccac112fa3eb6f
Phoebe Grosser <phoebegrosser@Phoebes-MacBook-Pro.local>
2023-01-13 09:33:36 +1100

    Progress

--- laplace_bc.jl
+++ laplace_bc.jl
@@ -12,7 +12,9 @@ x = OffsetArray(x, -2)
 # Only applied to interior
 f = cos.(5*pi/(2*Lx).*x[0:nx])
 
-p = spzeros(nx,1)
+A = spzeros(nx,nx)
+p = spzeros(nx)
+e = zeros(nx)
 
 # To check -> if BC is applied to all basis vectors, it will also be applied to eta?
 
@@ -20,23 +22,51 @@ function laplacian!(p, e, nx)
 
     p .= 0
 
-    for k in 0:(nx - 1)
+    for k in 1:(nx - 1)
         p[k] = (e[k + 1] + e[k - 1] - 2*e[k])/(dx^2)
     end
 
     return nothing
 end
 
-function dirichlet_bc(nx)
+function dirichlet_bc(p, nx)
+    # Dirichlet BCs applied to RHS of domain
+    p[nx] = 0
 
+    return nothing
 end
 
-function neumann_bc(nx)
+function neumann_bc(p, nx)
+    # Neumann BCs applied to LHS of domain
+    p[-1] = p[1]
 
+    return nothing
 end
-
-e = zeros(nx + hx)
 
 function generate_A!(A, nx, e)
 
+    u = 0
 
+    # Generate basis vectors
+    for i in 1:nx
+        e .= 0
+        e[i] = 1.0
+
+        u += 1
+
+        # Apply BCs
+        laplacian!(p, e, nx)
+
+        dirichlet_bc(p, nx)
+
+        neumann_bc(p, nx)
+
+        A[:, u] = p
+
+    end
+
+    return nothing
+
+end
+
+generate_A!(A, nx, e)
