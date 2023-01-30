68d9fdc89c2b9a6181e38a89e18d73c9ba719e38
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-30 15:59:10 +1100

    solves Poisson eq for few different BCs

--- laplace_bc_wOceananigans.jl
+++ laplace_bc_wOceananigans.jl
@@ -1,159 +1,197 @@
+using SparseArrays
+using Statistics
+using SpecialFunctions
 using LinearAlgebra
-using GLMakie
-Makie.inline!(true)
-
 
 using Oceananigans
-
-using Oceananigans.Architectures: child_architecture, device, arch_array
-using Oceananigans.Operators: ∇²ᶜᶜᶜ, ∇²ᶠᶜᶜ
+using Oceananigans.Architectures: architecture, device_event, device
 using Oceananigans.Utils: launch!
 using Oceananigans.BoundaryConditions: fill_halo_regions!
+using Oceananigans.Operators: ∇²ᶜᶜᶜ
+using Oceananigans.Grids: new_data
 using Oceananigans.Solvers: solve!,
-                            FFTBasedPoissonSolver,
                             PreconditionedConjugateGradientSolver,
                             MultigridSolver,
                             finalize_solver!
 
-using BenchmarkTools,
-      CUDA,
-      IterativeSolvers
-# using Oceananigans.Solvers: initialize_AMGX, finalize_AMGX
+using KernelAbstractions: @kernel, @index
 
-using KernelAbstractions: @kernel, @index, Event
-using Statistics: mean
+import Oceananigans.Solvers: initialize_matrix
+import Base: similar
 
-import Oceananigans.Solvers: precondition!
-
-
-Lx = 3.0
-nx = 20 # Ordinary points
-hx = 1    # Halo points
-
-arch = CPU()
-
-grid = RectilinearGrid(topology = (Bounded, Flat, Flat), 
-                       size = nx,
-                       x = (0, Lx),
-                       halo=hx)
-
- Nx, Ny, Nz = grid.Nx, grid.Ny, grid.Nz
-
-boundary_conditions = (west = GradientBoundaryCondition(0), east=ValueBoundaryCondition(0), north=nothing,south=nothing, top=nothing, bottom=nothing, immersed=nothing)
-
-LX, LY, LZ = Face, Center, Center
-
-# for plotting
-x = xnodes(LX, grid)
-
-# Select RHS
-f = Field{LX, LY, LZ}(grid; boundary_conditions)
-
-f₀(x, y, z) = cos(5π / (2Lx) * x)
-f₀(x, y, z) = sin(2π / Lx * x)
-
-set!(f, f₀)
-
-fill_halo_regions!(f)
-
-fig = Figure()
-ax1 = Axis(fig[1, 1], xlabel="x", title="rhs")
-ax2 = Axis(fig[2, 1], xlabel="x", title="solution")
-
-lines!(ax1, x, interior(f, :, 1, 1), linewidth=3, label="rhs")
-current_figure()
-
-η_analytical = Field{LX, LY, LZ}(grid; boundary_conditions)
-
-η(x, y, z) = - (2Lx / (5π))^2 * f₀(x, y, z)
-
-set!(η_analytical, η)
-
-fill_halo_regions!(η_analytical)
-
-lines!(ax2, x, interior(η_analytical, :, 1, 1), linewidth=3, label="truth")
-
-current_figure()
+using GLMakie
+Makie.inline!(true)
 
 
 
+# my attempt to enforce boundary conditions to pass along
+function Base.similar(f::Field, grid=f.grid)
+    loc = location(f)
+    return Field(loc,
+                 grid,
+                 new_data(eltype(parent(f)), grid, loc, f.indices),
+                 deepcopy(f.boundary_conditions),
+                 f.indices,
+                 f.operand,
+                 deepcopy(f.status))
+end
 
+# this won't be necessary when https://github.com/CliMA/Oceananigans.jl/pull/2885 is merged
+function initialize_matrix(::CPU, template_field, linear_operator!, args...; boundary_conditions=nothing)
+
+    loc = location(template_field)
+    Nx, Ny, Nz = size(template_field)
+    grid = template_field.grid
+
+    A = spzeros(eltype(grid), Nx*Ny*Nz, Nx*Ny*Nz)
+    make_column(f) = reshape(interior(f), Nx*Ny*Nz)
+
+    if boundary_conditions === nothing
+        boundary_conditions = FieldBoundaryConditions(grid, loc, template_field.indices)
+    end
+
+    eᵢⱼₖ = Field(loc, grid; boundary_conditions)
+    ∇²eᵢⱼₖ = similar(template_field)
+
+    for k = 1:Nz, j in 1:Ny, i in 1:Nx
+        parent(eᵢⱼₖ) .= 0
+        parent(∇²eᵢⱼₖ) .= 0
+        eᵢⱼₖ[i, j, k] = 1
+        fill_halo_regions!(eᵢⱼₖ)
+        linear_operator!(∇²eᵢⱼₖ, eᵢⱼₖ, args...)
+
+        A[:, Ny*Nx*(k-1) + Nx*(j-1) + i] .= make_column(∇²eᵢⱼₖ)
+    end
+    
+    return A
+end
+
+# the function we give to the solvers
+function compute_∇²!(∇²φ, φ)
+    grid = φ.grid
+    arch = architecture(grid)
+
+    # parent(φ) .-= mean(φ)
+    fill_halo_regions!(φ)
+    event = launch!(arch, grid, :xyz, ∇²!, ∇²φ, grid, φ, dependencies=device_event(arch))
+    wait(device(arch), event)
+    return nothing
+end
 
 @kernel function ∇²!(∇²f, grid, f)
     i, j, k = @index(Global, NTuple)
     @inbounds ∇²f[i, j, k] = ∇²ᶜᶜᶜ(i, j, k, grid, f)
 end
 
-function compute_∇²!(∇²φ, φ, arch, grid)
-    fill_halo_regions!(φ)
-    child_arch = child_architecture(arch)
-    event = launch!(child_arch, grid, :xyz, ∇²!, ∇²φ, grid, φ, dependencies=Event(device(child_arch)))
-    wait(device(child_arch), event)
-    fill_halo_regions!(∇²φ)
 
-    return nothing
-end
+# Now let's construct a grid and play around
+arch = CPU()
 
+#=
+grid = RectilinearGrid(arch,
+                       size = (100, 1, 1),
+                       x = (-1, 1),
+                       y = (0, 1),
+                       z = (0, 1),
+                       halo = (1, 1, 1),
+                       topology = (Bounded, Periodic, Periodic))
+=#
 
+grid = RectilinearGrid(arch,
+                       size = 300,
+                       x = (-1, 1),
+                       topology = (Bounded, Flat, Flat))
 
-∇²η_analytical = Field{LX, LY, LZ}(grid; boundary_conditions)
-compute_∇²!(∇²η_analytical, η_analytical, arch, grid)
-
-fill_halo_regions!(∇²η_analytical)
-
-lines!(ax1, x, interior(∇²η_analytical, :, 1, 1), linewidth=3, label="∇² truth")
-axislegend(ax1)
-
-current_figure()
-
-
-
-
-# Solve ∇²η = f with `PreconditionedConjugateGradientSolver`
-φ_cg = Field{LX, LY, LZ}(grid; boundary_conditions)
-φ_cg .= 0.9η_analytical
-
-cg_solver = PreconditionedConjugateGradientSolver(compute_∇²!, template_field=f)
-
-@info "Solving the Poisson equation with a conjugate gradient iterative solver..."
-@time solve!(φ_cg, cg_solver, f, arch, grid)
-
-fill_halo_regions!(φ_cg)
-
-lines!(ax2, x, interior(φ_cg, :, 1, 1), linewidth=3, label="CG")
+loc = (Center, Center, Center)
+boundary_conditions = FieldBoundaryConditions(grid, loc,
+                                              west = ValueBoundaryCondition(0),
+                                              east = ValueBoundaryCondition(0))
 
 
-# Solve ∇²η = f with `MultigridSolver`
-φ_mg = Field{LX, LY, LZ}(grid; boundary_conditions)
-φ_mg .= 0.9η_analytical
+
+σ = 8
+
+# a symetric solution with zero mean for φ(-1) = φ(+1)=0
+rhs(x, y, z) = x * exp(-σ^2 * x^2)
+φ(x, y, z) = √π * (x * erf(σ) - erf(σ * x)) / (4σ^3)
+
+# an assymetric solution for φ(-1) = φ(+1)=0
+rhs(x, y, z) = (x - 1/4) * exp(- σ^2 * (x - 1/4)^2)
+φ(x, y, z) = √π * ((1 + x) * erf(3σ/4) + (x - 1)* erf(5σ/4) + 2 * erf(σ/4 - x * σ)) / (8σ^3)
+
+# a symetric solution with zero mean for φ'(-1) = φ'(+1)=0
+# rhs(x, y, z) = x * exp(-σ^2 * x^2)
+# φ(x, y, z) = x * exp(-σ^2) / (2σ^2) - √π * erf(x * σ) / (4σ^3)
+
+
+# Solve ∇²φ = r
+φ_truth = CenterField(grid; boundary_conditions)
+
+# Initialize zero-mean "truth" solution
+set!(φ_truth, φ)
+fill_halo_regions!(φ_truth)
+
+
+# Calculate Laplacian of "truth"
+r = CenterField(grid)
+set!(r, rhs)
+parent(r) .-= mean(r) # not sure we need this
+fill_halo_regions!(r)
+
+
+A = initialize_matrix(CPU(), φ_truth, compute_∇²!; boundary_conditions)
+# @show eigvals(collect(A))
+
+
+φ_mg = CenterField(grid; boundary_conditions)
+
+# Now solve numerically via MG or CG solvers
 
 @info "Constructing an Algebraic Multigrid solver..."
-@time mgs = MultigridSolver(compute_∇²!, arch, grid; template_field = f)
+@time mgs = MultigridSolver(compute_∇²!, template_field=φ_mg)
 
-@info "Solving the Poisson equation with the Algebraic Multigrid solver..."
-@time solve!(φ_mg, mgs, f)
-
+@info "Solving with the Algebraic Multigrid solver..."
+@time solve!(φ_mg, mgs, r)
 fill_halo_regions!(φ_mg)
 
-finalize_solver!(mgs)
+# Solve Poisson equation
+φ_cg = CenterField(grid; boundary_conditions)
+
+@info "Constructing a Preconditioned Congugate Gradient solver..."
+@time cg_solver = PreconditionedConjugateGradientSolver(compute_∇²!, template_field=φ_cg, reltol=eps(eltype(grid)))
+
+@info "Solving with the Preconditioned Congugate Gradient solver..."
+@time solve!(φ_cg, cg_solver, r)
+fill_halo_regions!(φ_cg)
+
+
+# Compute the ∇²φ to see how good it matches the right-hand-side
+∇²φ_cg = CenterField(grid)
+compute_∇²!(∇²φ_cg, φ_cg)
+fill_halo_regions!(∇²φ_cg)
+
+∇²φ_mg = CenterField(grid)
+compute_∇²!(∇²φ_mg, φ_mg)
+fill_halo_regions!(∇²φ_mg)
+
+
+# Now plot
+
+x, y, z = nodes(r)
+
+fig = Figure()
+ax1 = Axis(fig[1, 1], xlabel="x", ylabel="∇²φ")
+lines!(ax1, x, interior(r, :, 1, 1), linewidth=6, label="truth")
+lines!(ax1, x, interior(∇²φ_mg, :, 1, 1), linewidth=3, label="MG")
+lines!(ax1, x, interior(∇²φ_cg, :, 1, 1), linewidth=3, linestyle=:dash, label="CG")
+axislegend(ax1)
+
+ax2 = Axis(fig[2, 1], xlabel="x", ylabel="φ")
+lines!(ax2, x, interior(φ_truth, :, 1, 1), linewidth=6, label="truth")
 lines!(ax2, x, interior(φ_mg, :, 1, 1), linewidth=3, label="MG")
-
-
-# Solve ∇²η = f with `FFTBasedPoissonSolver`
-φ_fft = Field{LX, LY, LZ}(grid; boundary_conditions)
-φ_fft .= 0.9*η_analytical
-
-fft_solver = FFTBasedPoissonSolver(grid)
-fft_solver.storage .= interior(f)
-
-@info "Solving the Poisson equation with an FFT-based solver..."
-@time solve!(φ_fft, fft_solver, fft_solver.storage)
-fill_halo_regions!(φ_fft)
-
-lines!(ax2, x, interior(φ_fft, :, 1, 1), linewidth=3, label="FFT")
-
+lines!(ax2, x, interior(φ_cg, :, 1, 1), linewidth=3, linestyle=:dash, label="CG")
 axislegend(ax2)
 
-ylims!(ax2, (-1.1, 1.1))
-
+max_r = maximum(abs.(r))
+ylims!(ax1, (-1.2*max_r, 1.2max_r))
 current_figure()
